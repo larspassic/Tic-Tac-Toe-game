567d40c12c499deea378d37457ab7a1157da846f
Lars Passic <larspassic@outlook.com>
2021-03-08 15:44:47 -0800

    Write first notes

---
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+
+            //Display the board to the user
+
+
+            //Ask the user to choose a number
+            Console.WriteLine($"Please type a number to make your move: (1-9)");
+
+            //Receive the input from the user
+            int userInput = 0;
+            userInput = Console.Read();
+
+
+
+        }
+    }
+}
