50d7159618c56b44e0c617758ead35911c85d9d3
Lars Passic <larspassic@outlook.com>
2021-03-11 17:29:28 -0800

    Just trying to sketch out a basic design
    Not really sure what to use as my data structure to hold the slots

--- Tic Tac Toe/Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -4,13 +4,27 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
+        //Variables
+        string slot1 = "";
+        string slot2 = "";
+        string slot3 = "";
+        string slot4 = "";
+        string slot5 = "";
+        string slot6 = "";
+        string slot7 = "";
+        string slot8 = "";
+        string slot9 = "";
+
         static void Main(string[] args)
         {
             //Welcome
             Console.WriteLine("Welcome to Tic Tac Toe!");
 
-            //Display the board to the user
 
+
+
+            //Display the board to the user
+            DisplayBoard();
 
             //Ask the user to choose a number
             Console.WriteLine($"Please type a number to make your move: (1-9)");
@@ -21,6 +35,13 @@ namespace Tic_Tac_Toe
 
 
 
+        }
+
+        private static void DisplayBoard()
+        {
+            Console.WriteLine($"{slot1} | {slot2} | {slot3}");
+            Console.WriteLine($"{slot4} | {slot5} | {slot6}");
+            Console.WriteLine($"{slot7} | {slot8} | {slot9}");
         }
     }
 }
