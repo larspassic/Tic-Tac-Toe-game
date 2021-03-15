28627abcb1d1e2c26c261752c52541ad2f46c82f
Lars Passic <43658463+larspassic@users.noreply.github.com>
2021-03-15 12:54:50 -0700

    Update Program.cs
    Added a switch statement and changed away from using static

--- Tic Tac Toe/Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -15,32 +15,75 @@ namespace Tic_Tac_Toe
         string slot8 = "";
         string slot9 = "";
 
-        static void Main(string[] args)
+        void Main(string[] args)
         {
             //Welcome
-            Console.WriteLine("Welcome to Tic Tac Toe!");
+            Console.WriteLine("Welcome to Tic Tac Toe!\n");
+
+            string playerTurn = "X";
+
+            //Main game loop
+            while (true)
+            {
 
 
 
+                //Display the board to the user
+                DisplayBoard();
 
-            //Display the board to the user
-            DisplayBoard();
+                //Ask the user to choose a number
+                Console.WriteLine($"Please type a number to make your move: (1-9)");
 
-            //Ask the user to choose a number
-            Console.WriteLine($"Please type a number to make your move: (1-9)");
+                //Receive the input from the user
+                int userInputInt = 0;
+                string userInputString = Console.ReadLine();
+                userInputInt = int.Parse(userInputString);
 
-            //Receive the input from the user
-            int userInput = 0;
-            userInput = Console.Read();
+                //Add the character of the player to the slot
+                
+                switch (userInputInt)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    
+                    default:
+                        break;
+                }
 
-
-
+                //End of the game loop, switch turns
+                if (playerTurn == "X")
+                {
+                    playerTurn = "O";
+                }
+                else if (playerTurn == "O")
+                {
+                    playerTurn = "X";
+                }
+            }
         }
 
-        private static void DisplayBoard()
+        private void DisplayBoard()
         {
             Console.WriteLine($"{slot1} | {slot2} | {slot3}");
+            Console.WriteLine($"---------------------------");
             Console.WriteLine($"{slot4} | {slot5} | {slot6}");
+            Console.WriteLine($"---------------------------");
             Console.WriteLine($"{slot7} | {slot8} | {slot9}");
         }
     }
